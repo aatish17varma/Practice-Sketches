06338353ab9c7030e2328cee058149461b4f4537
Aatish Varma <aatishvarma@10-19-162-89.dynapool.wireless.nyu.edu>
2019-01-08 17:57:00 -0500

    added if statement method

---
+++ complicated-if-statements.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <assert.h>
+#include <iostream>
+using namespace std;
+#include "vops.h"
+#include "complicated-if-statements.h"
+namespace ANONYMOUS{
+
+void complicatedIfs(int& _out) {
+  _out = 1;
+  return;
+}
+void returnsOne(int& _out) {
+  _out = 1;
+  return;
+}
+
+}
---
+++ complicated-if-statements.h
@@ -0,0 +1,15 @@
+#ifndef COMPLICATED-IF-STATEMENTS_H
+#define COMPLICATED-IF-STATEMENTS_H
+
+#include <cstring>
+
+#include "vops.h"
+
+namespace ANONYMOUS{
+}
+namespace ANONYMOUS{
+extern void complicatedIfs(int& _out);
+extern void returnsOne(int& _out);
+}
+
+#endif
